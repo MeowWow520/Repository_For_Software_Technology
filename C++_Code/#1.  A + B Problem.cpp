9e8d4700cf2ca89cbda867a8a23ef6fc76c13e46
MeowWow520 <128897166+MeowWow520@users.noreply.github.com>
2025-04-11 18:15:32 +0800

    Add files via upload

---
+++ C++_Code/#1.  A + B Problem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/** #1. A + B Problem
+ *  https://uoj.ac/problem/1
+ * 
+ *  ------问题描述------
+ *  给你两个数α和b, 请输出他们的和
+
+ *  ------输入格式------
+ *  一行, 两个用空格隔开的整数 a 和 b
+
+ *  ------输出格式------
+ *  一个整数, 表示 a + b
+ */
+
+int a, b;
+int main() {
+	cin >> a >> b;
+	cout << a + b;
+	return 0;
+}
